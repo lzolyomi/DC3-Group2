5890f8056f456d7c1ad236da6fce23ccd8c1bdbc
Teddy Kosciuszek <t.l.kosciuszek@student.tue.nl>
2021-10-02 12:45:35 +0200

    Did a translate: vertaald voor de engelstalingen

--- models/calc_risk_score.R
+++ models/calc_risk_score.R
@@ -1,9 +1,10 @@
 #' Bereken de risico_categorie, gebaseerd op begroeiing
+# Calculate risk category based on vegetation
 #'
-#' @param stuwvak naam van het stuwvak
-#' @param risk_date Datum van risico-inschatting
+#' @param stuwvak (name of the weir compartment), naam van het stuwvak
+#' @param risk_date (Date of the risk assessment), Datum van risico-inschatting
 #'
-#' @return Risico-categorie van de begroeiing en 25, 50 en 75% quantiles
+#' @return Risk category of the growth in 25, 50, and 75% quantiles, Risico-categorie van de begroeiing en 25, 50 en 75% quantiles
 #' @export
 calc_begroeiing_risk <- function(stuwvak, risk_date, feature_table_path = "data/feature_tables/", model_path = "output/models/") {
   stuwvak_data <- get_feature_table(stuwvak, feature_table_path)
@@ -19,7 +20,7 @@ calc_begroeiing_risk <- function(stuwvak, risk_date, feature_table_path = "data/
     select(VERSCHIL, Q)
 
   # Winter is coming
-  if (month(risk_date) <= 2 | month(risk_date) >= 10) { # zomerperiode van maart tot en met september
+  if (month(risk_date) <= 2 | month(risk_date) >= 10) { # summer from march until september,  zomerperiode van maart tot en met september
     huidig_begroeiing <- NA_integer_
   } else if (huidig_begroeiing$Q <= 0) {
     warning("No debietdata for ", stuwvak, " on date ", risk_date)
@@ -39,7 +40,7 @@ calc_begroeiing_risk <- function(stuwvak, risk_date, feature_table_path = "data/
       next
     }
     model_year <- read_rds(sprintf("%s%s_%i.rds", model_path, stuwvak, i))
-    data_year <- filter(stuwvak_data, YEAR == i & MAAND > 2 & MAAND < 10) # zomerperiode van maart tot en met september
+    data_year <- filter(stuwvak_data, YEAR == i & MAAND > 2 & MAAND < 10) # summer from march until september, zomerperiode van maart tot en met september
 
     pred_year <- predict(model_year, select(data_year, Q)) %>% pmax(0)
     begroeiing_year <- pmax(0, data_year$VERSCHIL - pred_year)
@@ -58,11 +59,13 @@ calc_begroeiing_risk <- function(stuwvak, risk_date, feature_table_path = "data/
 }
 
 #' Bereken aantal punten voor neerslagtekort
+# Calculate total points for the precipitation deficit
 #'
 #' @param neerslag_tekort percentage neerslagtekort berekent als
 #' `(waarde - mediaan) / abs(mediaan) * 100`
+# percentage precipitation deficit calculated as (value - median / {abs(median) *100})
 #'
-#' @return het aantal afvoer en aanvoerpunten
+#' @return number of discharge and supply points, het aantal afvoer en aanvoerpunten
 #' @export
 neerslagtekort_risico <- function(neerslag_tekort) {
   afvoer_points <- if_else(neerslag_tekort <= 10 & neerslag_tekort >= -10, 1,
@@ -80,10 +83,11 @@ neerslagtekort_risico <- function(neerslag_tekort) {
 }
 
 #' Bereken aantal punten voor grondwater
+#  Calculate number of points for groundwater
 #'
-#' @param grondwater grondwaterklasse (bijv. Droog)
+#' @param grondwater groundwater classification, grondwaterklasse (bijv. Droog)
 #'
-#' @return het aantal afvoer en aanvoerpunten
+#' @return number of discharge and supply points, het aantal afvoer en aanvoerpunten
 #' @export
 grondwater_risico <- function(grondwater) {
   afvoer_points <- if_else(grondwater == "Normaal", 1,
@@ -102,11 +106,13 @@ grondwater_risico <- function(grondwater) {
 }
 
 #' Bereken het aantal punten voor neerslag
-#'
+#' Calculate number of points for the precipitation
 #' @param neerslag_verwachting dataframe met de verwachting
 #' voor de komende 10 dagen
+# Dataframe with expectation for the coming 10 days
 #'
 #' @return het aantal afvoer en aanvoerpunten
+# Number of discharge and supply points
 #' @export
 neerslag_risico <- function(neerslag_verwachting) {
   natte_dagen <- neerslag_verwachting %>%
@@ -119,6 +125,7 @@ neerslag_risico <- function(neerslag_verwachting) {
     unique()
 
   # Opeenvolgende natte dagen
+  #Consecutive wet days
   if (length(natte_dagen) > 1) {
     nat_counter <- 1
     for (i in 2:length(natte_dagen)) {
@@ -133,6 +140,7 @@ neerslag_risico <- function(neerslag_verwachting) {
   }
 
   # Opeenvolgende erg natte dagen
+  #Consecutive very wet days
   if (length(erg_natte_dagen) > 1) {
     erg_nat_counter <- 1
     for (i in 2:length(erg_natte_dagen)) {
@@ -165,11 +173,14 @@ neerslag_risico <- function(neerslag_verwachting) {
 }
 
 #' Bereken het aantal punten voor temperatuur
+#Calculate the number of points for temperature
 #'
 #' @param temp_verwachting dataframe met de verwachting
 #' voor de komende 10 dagen
+#Dataframe with the expectation for the coming 10 days
 #'
 #' @return het aantal afvoer en aanvoerpunten (afvoer is altijd 0)
+# The number of discharge and supply points (discharge is always 0)
 #' @export
 temp_risico <- function(temp_verwachting) {
   temp_verwachting <-
@@ -187,11 +198,14 @@ temp_risico <- function(temp_verwachting) {
 }
 
 #' Bereken het aantal punten voor bewolking
+# Calculate the nuber of points for the cloud cover
 #'
 #' @param bewolking_verwachting dataframe met de verwachting
 #' voor de komende 10 dagen
+#Dataframe with the expectation for the coming 10 days
 #'
 #' @return het aantal afvoer en aanvoerpunten (afvoer is altijd 0)
+#The number of discharge and supply points, discharge is always 0
 #' @export
 bewolking_risico <- function(bewolking_verwachting) {
   dagen_minder_30 <- bewolking_verwachting %>%
@@ -209,10 +223,14 @@ bewolking_risico <- function(bewolking_verwachting) {
 }
 
 #' Bereken het aan/afvoer risico 
+#Calculate supply and discharge risk
+#This is not based on date, when for example the precipitation deficit is missing from 16-2-2021, the risk points for the precipitation deficit on 25-2-2021 is then based on the data from 16-2-2021
 #' Dit is niet op basis van datums. Ofwel wanneer bijvoorbeeld neerslagtekort ontbreekt vanaf 16-2-2021, de risicopunten voor neerslagtekort op 25-2-2021 is dan gebaseerd op de data van 16-2-2021
 #' @param stuwvak Stuwvak_naam
+#Weir name
 #'
 #' @return het aan- en afvoerrisico in punten
+#The supply and discharge risks in points
 #' @export
 calc_risk <- function(stuwvak, waterloop_naam,
                       grondwater_path = "data/preprocessed/grondwater.rds",
@@ -229,20 +247,25 @@ calc_risk <- function(stuwvak, waterloop_naam,
   waterloop_naam <- stringr::str_remove(waterloop_naam, "_")
 
   # Grondwater
+  # Groundwater
   grondwater <- read_rds(grondwater_path) %>%
     filter(!is.na(Klasse), Waterloop == waterloop_naam) %>%
     slice(n()) %>%
     pull(Klasse)
   
   # print(sprintf("grondwaterklasse %s: %s", waterloop_naam, grondwater))
+  #print( groundwater classification, waterway name, groundwater variable)
 
   grondwater_punten <- grondwater_risico(grondwater)
   afvoer_points <- afvoer_points + grondwater_punten[1]
   # print(sprintf("afvoerpunten %s: %s ", waterloop_naam, afvoer_points))
+  #print(discharge points, waterway name, discharge points variable)
   aanvoer_points <- aanvoer_points + grondwater_punten[2]
   # print(sprintf("aanvoerpunten %s: %s ", waterloop_naam, aanvoer_points))
+  # print( supply points, waterway name, supply point variable)
 
   # Neerslagtekort
+  #Precipitation deficit
   neerslag_tekort <- read_rds(neerslag_tekort_path) %>%
     filter(!is.na(huidig_jaar)) %>%
     slice(n()) %>%
@@ -254,6 +277,7 @@ calc_risk <- function(stuwvak, waterloop_naam,
   aanvoer_points <- aanvoer_points + neerslagtekort_punten[2]
 
   # Neerslagverwachting
+  #Expected precipitation deficit
   neerslag_verwachting <- read_rds(neerslag_path) %>%
     mutate(datum = as.Date(DateTime))
 
@@ -262,6 +286,7 @@ calc_risk <- function(stuwvak, waterloop_naam,
   aanvoer_points <- aanvoer_points + neerslag_punten[2]
 
   # Temperatuurverwachting
+  # Expected temperature
   temp_verwachting <- read_rds(temperatuur_path)
 
   temp_punten <- temp_risico(temp_verwachting)
@@ -269,6 +294,7 @@ calc_risk <- function(stuwvak, waterloop_naam,
   aanvoer_points <- aanvoer_points + temp_punten[2]
 
   # Bewolkingverwachting
+  # Expected cloud cover
   bewolking_verwachting <- read_rds(bewolking_path) %>%
     group_by(date) %>%
     summarise(gem_value = unique(gem_value))
@@ -278,6 +304,7 @@ calc_risk <- function(stuwvak, waterloop_naam,
   aanvoer_points <- aanvoer_points + bewolking_punten[2]
 
   # Overstort aanwezig
+  # Overflow events
   shape_stuwvak <- read_rds(waterlopen_path) %>%
     filter(STUWVAK == stuwvak) %>%
     st_transform(28992)
--- models/extrapolate_begroeiing.R
+++ models/extrapolate_begroeiing.R
@@ -1,11 +1,11 @@
 #' extrapolate_begroeiing
 #' Voorspellen van de begroeiing voor de aankomende 21 dagen, op basis van een lineair model. 
 #' 
-#' @param preds data nodig voor extrapolatie
-#' @param timesteps tijdstappen, default = 7
-#' @param gem_temp gemiddelde temperatuur de afgelopen tijd default = 22
+#' @param preds (data needed for extrapolation) data nodig voor extrapolatie
+#' @param timesteps (time intervals, default 7) tijdstappen, default = 7
+#' @param gem_temp (average temperature of the last time interval, defined above) gemiddelde temperatuur de afgelopen tijd default = 22
 #'
-#' @return dataframe met extrapoleerde data voor in plotly
+#' @return (dataframe with extrapolated data for plotly) dataframe met extrapoleerde data voor in plotly
 #' @export
 extrapolate_begroeiing <- function(preds, timesteps = 7, gem_temp = 22) {
   if (nrow(preds) <= timesteps) {
@@ -13,39 +13,49 @@ extrapolate_begroeiing <- function(preds, timesteps = 7, gem_temp = 22) {
   }
   
   # Ophalen data van de laatste 7 dagen
+  # Pickup the data from the last 7 days
   last_data <- preds %>%
     tail(timesteps) %>%
     mutate(ID = 1:timesteps)
   
   # Minstens twee waarden
+  # At minimum two growth values
   if (length(which(!is.na(last_data$BEGROEIING))) < 2) {
     return()
   }
   
   ## Laatste datum opzoeken en er 21 dagen bijop tellen
+  ## Calculate 21 day forecast period
   new_dates <- seq(last(last_data$TIME) + days(1), last(last_data$TIME) + days(21), 1)
   
   ## Op basis van de laatste 7 dagen via een lineair model model trainen
+  # Based on the last 7 days, train a linear model
   extrapolate_model <- lm(BEGROEIING ~ ID, data = last_data)
   if (extrapolate_model$coefficients["ID"] < 0) {
     return() # Model voorspeld afname van begroeiing...
-  }
+  }          # Predicted vegetation decline by the model
   
   ## 21 dagen begroeiing voorspellen op basis van het estrapolatie_model
+  # 21 day vegetation forecast from the extrapolated model
   new_vals <- predict(extrapolate_model, data.frame(ID = seq(timesteps + 1, timesteps + 21, 1)))
   
   # Niks teruggeven als de voorspelde begroeiing 0 blijft..
+  # Return nothing if the vegetation forecast is 0
   if (all(new_vals == 0)) {
     return()
   }
   
   # Met warm weer: groei +20%
+  # With warm weather increase the growth 20%
+
   # Koud weer: groei -20%
+  # With cold weather decrease growth 20%
   if (!is.na(gem_temp) & gem_temp > 25) {
     new_vals <- new_vals * 1.2 ## TODO: Check Wat is de bron van factor 1.2?
+                                #Check what the source of 1.2 value is
   } else if (!is.na(gem_temp) & gem_temp < 20) {
     new_vals <- new_vals * 0.8 ## TODO: Check Wat is de bron van factor 0.8>
-  }
+  }                            #Check what the source of value 0.8 is
   
   tibble(TIME = new_dates, PREDS = pmax(new_vals, 0)) %>%
     return()
