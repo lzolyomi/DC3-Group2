fb7e740a91c629ebd7340bf1aed47cfaaa888f2e
Teddy Kosciuszek <t.l.kosciuszek@student.tue.nl>
2021-10-11 16:03:15 +0200

    Fixt window_len

--- src/st_app.py
+++ src/st_app.py
@@ -178,9 +178,13 @@ if func == "Dataframes":
 
 if func == "Mowing Plots":
     comp = st.sidebar.selectbox("Select the weir compartment",compartments) #compartment
-    wind_len = st.sidebar.number_input("Filter Window Length (Days)", min_value = 3, step = 2)
-    polorder = st.sidebar.number_input("Smoothing Approximation Order", min_value= 1, step=1)
-    plot_filtered(filter_data(df = df_waterway, weir = comp, window_length=wind_len, polyorder=polorder,  derivative = 1, ))
+    wind_len = st.sidebar.number_input("Filter Window Length (Days)", min_value = 3, step = 2, value = 201)
+    polorder = st.sidebar.number_input("Smoothing Approximation Order", min_value= 1, step=1, value = 5)
+    wind = wind_len
+    if wind % 2 == 0:
+        wind -= 1
+        #wind_len = st.sidebar.number_input("Filter Window Length (Days)", min_value = 3, step = 2, value = wind)
+    plot_filtered(filter_data(df = df_waterway, weir = comp, window_length=wind, polyorder=polorder,  derivative = 1))
         
 
 if func == "Kepler Maps":
